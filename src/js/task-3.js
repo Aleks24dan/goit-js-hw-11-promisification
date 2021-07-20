2a8880a6ba79bf77b93b3cf0dbf1c29a569c317a
Aleks24dan <aleks24dan@gmail.com>
2021-07-20 07:32:09 +0200

    1

--- src/js/task-3.js
+++ src/js/task-3.js
@@ -3,44 +3,45 @@ const randomIntegerFromInterval = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-const makeTransaction = ({ id, amount }) => {
+const makeTransaction = (transaction) => {
     const delay = randomIntegerFromInterval(200, 500);
 
     return new Promise((resolve, reject) => {
+        const canProcess = Math.random() > 0.3;
+
         setTimeout(() => {
-            const canProcess = Math.random() > 0.3;
             if (canProcess) {
-                resolve(`Transaction ${id} processed in ${amount}ms`);
+                resolve(transaction.id, transaction.time);
             } else {
-                reject(`Error processing transaction ${id}. Please try again later.`);
+                reject(transaction.id);
             }
         }, delay);
 
     });
 }
 
-const logSuccess = result => {
-    console.log(result);
+const logSuccess = (id , time) => {
+    console.log(`Transaction ${id} processed in ${time}ms`);
 };
 
-const logError = result => {
-    console.warn(result);
+const logError = (id) => {
+    console.warn(`Error processing transaction ${id}. Please try again later.`);
 };
 
 
-makeTransaction({ id: 70, amount: 150 })
+makeTransaction({ id: 70, time: 150 })
     .then(logSuccess)
     .catch(logError);
 
-makeTransaction({ id: 71, amount: 230 })
+makeTransaction({ id: 71, time: 230 })
     .then(logSuccess)
     .catch(logError);
 
-makeTransaction({ id: 72, amount: 75 })
+makeTransaction({ id: 72, time: 75 })
     .then(logSuccess)
     .catch(logError);
 
-makeTransaction({ id: 73, amount: 100 })
+makeTransaction({ id: 73, time: 100 })
     .then(logSuccess)
     .catch(logError);
 
